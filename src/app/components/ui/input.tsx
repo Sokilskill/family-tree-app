4599cd328e90a9e4ea1d997d377c30e3317aa17e
sokilskills <vprovorotov@amil.com>
2026-04-14 17:53:15 +0300

    style: add class variance authority

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,25 +1,36 @@
 import * as React from "react";
 
 import { cn } from "./utils";
+import { cva, VariantProps } from "class-variance-authority";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        type={type}
-        data-slot="input"
-        className={cn(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base bg-input-background transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-          className,
-        )}
-        {...props}
-      />
-    );
+const inputVariants = cva(
+  "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md  px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm border border-slate-200 bg-white px-3 py-2 shadow-sm aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+  {
+    variants: {
+      variant: {
+        addNewForm: "rounded-xl",
+      },
+    },
+    defaultVariants: {
+      variant: "addNewForm",
+    },
   },
 );
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input"> & VariantProps<typeof inputVariants>
+>(({ className, variant, type, ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      type={type}
+      data-slot="input"
+      className={cn(inputVariants({ variant, className }))}
+      {...props}
+    />
+  );
+});
 
 Input.displayName = "Input";
 
